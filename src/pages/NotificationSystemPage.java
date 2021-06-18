dcc6c41189e672beb646fcfc55c595f7a2118e9f
MarijaTodorov <marija.todorovic9@gmail.com>
2021-06-18 15:03:30 +0200

    Created NotificationSystemPage

--- src/pages/NotificationSystemPage.java
+++ src/pages/NotificationSystemPage.java
@@ -1,5 +1,29 @@
 package pages;
 
-public class NotificationSystemPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+public class NotificationSystemPage extends BasicPage {
+
+	public NotificationSystemPage(WebDriver driver, JavascriptExecutor js, WebDriverWait waiter) {
+		super(driver, js, waiter);
+	}
+
+	public WebElement getMessage() {
+		return driver.findElement(By.xpath(
+				"//*[contains(@class, 'alert--success') or contains(@class, 'alert--danger')][contains(@style,'display: block')]"));
+	}
+
+	public String getMessageTxt() {
+		return this.getMessage().getText();
+	}
+
+	public void waitUntilMessageDisappears() {
+		this.waiter.until(ExpectedConditions.attributeContains(By.xpath(" //*[contains(@class, 'system_message')]"),
+				"style", "display: none;"));
+	}
 }
