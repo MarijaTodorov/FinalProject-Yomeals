33ebf43e93cb473694b20e8c71df54e2fac907a4
MarijaTodorov <marija.todorovic9@gmail.com>
2021-06-18 12:15:24 +0200

    Created BasicPage

--- src/pages/BasicPage.java
+++ src/pages/BasicPage.java
@@ -1,5 +1,18 @@
 package pages;
 
-public class BasicPage {
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+public abstract class BasicPage {
+	protected WebDriver driver;
+	protected JavascriptExecutor js;
+	protected WebDriverWait waiter;
+
+	public BasicPage(WebDriver driver, JavascriptExecutor js, WebDriverWait waiter) {
+		super();
+		this.driver = driver;
+		this.js = js;
+		this.waiter = waiter;
+	}
 }
