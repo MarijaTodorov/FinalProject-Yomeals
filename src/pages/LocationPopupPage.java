417d2a97d0e565c677eb7658bdf073bac7677d98
MarijaTodorov <marija.todorovic9@gmail.com>
2021-06-18 12:53:15 +0200

    Created LocationPopupPage

--- src/pages/LocationPopupPage.java
+++ src/pages/LocationPopupPage.java
@@ -1,5 +1,51 @@
 package pages;
 
-public class LocationPopupPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LocationPopupPage extends BasicPage {
+
+	public LocationPopupPage(WebDriver driver, JavascriptExecutor js, WebDriverWait waiter) {
+		super(driver, js, waiter);
+	}
+
+	public WebElement getLocationHeader() {
+		return driver.findElement(By.className("location-selector"));
+	}
+
+	public WebElement getCloseElement() {
+		return driver.findElement(By.className("close-btn-white"));
+	}
+
+	public WebElement getKeyword() {
+		return driver.findElement(By.xpath("//*[@id='locality_keyword']"));
+	}
+
+	public WebElement getLocationItem() {
+		return driver.findElement(By.xpath("//li/a[contains(text(), '\" + locationName + \"')]/.."));
+	}
+
+	public WebElement getLocationInput() {
+		return driver.findElement(By.xpath("//*[@id='location_id']"));
+	}
+
+	public WebElement getSubmit() {
+		return driver.findElement(By.xpath("//*[@name='btn_submit']"));
+	}
+
+	public void openPopup() {
+		this.getLocationHeader().click();
+	}
+
+	public void setLocation(String locationName) {
+		this.getKeyword().click();
+		String value = this.getLocationItem().getAttribute("data-value");
+		js.executeScript("arguments[0].value=arguments[1]", this.getLocationInput(), value);
+		js.executeScript("arguments[0].click()", this.getSubmit());
+
+	}
 
 }
